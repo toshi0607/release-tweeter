d4f86116d469c0c04488c25f3b0241ae6c8abbf5
toshi0607 <s.toshi0607@gmail.com>
2018-05-20 18:40:58 +0900

    :green_heart: remove NW dependency when testing

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
-	"github.com/ChimeraCoder/anaconda"
+	a "github.com/ChimeraCoder/anaconda"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -20,7 +21,12 @@ var (
 	TWITTER_ACCESS_TOKEN_SECRET = os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
 	TWITTER_CONSUMER_KEY        = os.Getenv("TWITTER_CONSUMER_KEY")
 	TWITTER_CONSUMER_SECRET     = os.Getenv("TWITTER_CONSUMER_SECRET")
+	postTweetFunc               = func(api a.TwitterApi) postTweet {
+		return api.PostTweet
+	}
 )
+
+type postTweet func(status string, v url.Values) (tweet a.Tweet, err error)
 
 func main() {
 	lambda.Start(Handler)
@@ -41,14 +47,17 @@ func Handler() {
 
 	tag := getLatestTag(resp.Request.URL.Path)
 
-	api := anaconda.NewTwitterApiWithCredentials(
+	api := a.NewTwitterApiWithCredentials(
 		TWITTER_ACCESS_TOKEN,
 		TWITTER_ACCESS_TOKEN_SECRET,
 		TWITTER_CONSUMER_KEY,
 		TWITTER_CONSUMER_SECRET,
 	)
 	message := fmt.Sprintf("%s %s released! check the new features on GitHub.\n%s", REPO, tag, REPO_URL)
-	_, err = api.PostTweet(message, nil)
+	tweet, err := postTweetFunc(*api)(message, nil)
+	if !strings.Contains(tweet.Text, tag) {
+		log.Fatalf("faile to tweet: %s", tweet.Text)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
--- main_test.go
+++ main_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"net/url"
+	"testing"
+
+	a "github.com/ChimeraCoder/anaconda"
+)
 
 func TestHandler(t *testing.T) {
+	postTweet = func(api a.TwitterApi) postTweetFunc {
+		return postTweetHelper
+	}
 	Handler()
+}
+
+func postTweetHelper(s string, v url.Values) (a.Tweet, error) {
+	return a.Tweet{Text: s}, nil
 }
