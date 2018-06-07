b67b1269767ecc676b226517dad6d664be5c7457
toshi0607 <s.toshi0607@gmail.com>
2018-06-07 10:53:23 +0900

    :green_heart: add test case to main

--- main_test.go
+++ main_test.go
@@ -10,17 +10,26 @@ import (
 func TestHandler(t *testing.T) {
 	prepare()
 
-	res, err := handler(events.APIGatewayProxyRequest{
-		HTTPMethod: "POST",
-		Body:       "{\"owner\": \"toshi0607\", \"repo\": \"gig\"}",
-	})
-
-	if err != nil {
-		t.Errorf("got an error %v, want nothing happened", err)
+	tests := []struct {
+		label, owner, repo, method string
+		status                     int
+	}{
+		{"normal", "toshi0607", "gig", "POST", http.StatusOK},
+		{"invalid req", "", "gig", "POST", http.StatusBadRequest},
+		{"invalid repo", "toshi0607", "nothing", "POST", http.StatusInternalServerError},
+		{"invalid method", "toshi0607", "gig", "GET", http.StatusBadRequest},
 	}
 
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("ExitStatus=%d, want %d", res.StatusCode, http.StatusOK)
+	for _, te := range tests {
+
+		res, _ := handler(events.APIGatewayProxyRequest{
+			HTTPMethod: te.method,
+			Body:       "{\"owner\": \"" + te.owner + "\", \"repo\": \"" + te.repo + "\"}",
+		})
+
+		if res.StatusCode != te.status {
+			t.Errorf("ExitStatus=%d, want %d", res.StatusCode, te.status)
+		}
 	}
 }
 
